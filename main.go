4aea3a78c8b2f347b2ec06bf2f638e48ca727531
mofodox <khairulakmal.dev@gmail.com>
2022-10-27 13:22:47 +0800

    update recipe endpoint

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 
 	router.POST("/api/v1/recipes", NewRecipeHandler)
 	router.GET("/api/v1/recipes", ListAllRecipesHandler)
+	router.PUT("/api/v1/recipes/:id", UpdateRecipeHandler)
 
 	router.Run()
 }
@@ -65,4 +66,37 @@ func NewRecipeHandler(c *gin.Context) {
 
 func ListAllRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+
+		return
+	}
+
+	recipes[index] = recipe
+
+	c.JSON(http.StatusOK, recipe)
 }
