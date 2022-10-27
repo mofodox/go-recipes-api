caf36ba0ed8878ff055e2022d8221be1cfa9c90c
mofodox <khairulakmal.dev@gmail.com>
2022-10-27 13:38:52 +0800

    search recipe by tag

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -43,6 +44,7 @@ func main() {
 	router.GET("/api/v1/recipes", ListAllRecipesHandler)
 	router.PUT("/api/v1/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/api/v1/recipes/:id", DeleteRecipeHandler)
+	router.GET("/api/v1/recipes/search", SearchRecipesHandler)
 
 	router.Run()
 }
@@ -127,4 +129,26 @@ func DeleteRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Recipe has been deleted",
 	})
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+
+	listOfRecipes := make([]Recipe, 0)
+
+	for i := 0; i < len(recipes); i++ {
+		found := false
+
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
+		}
+	}
+
+	c.JSON(http.StatusOK, listOfRecipes)
 }
