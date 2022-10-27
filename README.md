41880cb4063e5b6218072c5b908944e0ef3798d7
mofodox <khairulakmal.dev@gmail.com>
2022-10-27 11:57:38 +0800

    new recipe endpoint

---
+++ README.md
@@ -0,0 +1,13 @@
+# Recipes API
+
+This is a project on the recipes API
+
+## Endpoints
+
+| HTTP Method | Resource                     | Description                  |
+| ----------- | ---------------------------- | ---------------------------- |
+| GET         | /api/v1/recipes              | Returns a list of recipes    |
+| POST        | /api/v1/recipes              | Creates a new recipe         |
+| PUT         | /api/v1/recipes/:id          | Updates an existing recipe   |
+| DELETE      | /api/v1/recipes/:id          | Deletes an existing recipe   |
+| GET         | /api/v1/recipes/search?tag=X | Searches for recipes by tags |
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,8 +17,35 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
 func main() {
 	router := gin.Default()
 
+	router.POST("/api/v1/recipes", NewRecipeHandler)
+
 	router.Run()
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+
+	recipes = append(recipes, recipe)
+
+	c.JSON(http.StatusCreated, recipe)
 }
